21b93d7fb49d6d4a824d97502f27e9f170b1ad2f
saturn <saturn900@gmail.com>
2016-10-17 22:35:30 +0700

    starting simple wicket app

--- server/src/main/java/WicketApplication.java
+++ server/src/main/java/WicketApplication.java
@@ -1,7 +1,12 @@
+import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
 
 /**
  * Created by saturn on 17.10.2016.
  */
 public class WicketApplication extends WebApplication {
+    @Override
+    public Class<? extends Page> getHomePage() {
+        return null;
+    }
 }
