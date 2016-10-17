a29160995875cc693a3d08f91584ec5cfaaf4caf
saturn <saturn900@gmail.com>
2016-10-18 05:07:39 +0700

    ++ label to Home

--- server/src/main/java/jd/toll/server/pages/Home.java
+++ server/src/main/java/jd/toll/server/pages/Home.java
@@ -1,10 +1,15 @@
 package jd.toll.server.pages;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
 
 /**
  * Created by saturn on 17.10.2016.
  */
 public class Home extends WebPage {
+
+    public Home() {
+        add(new Label("label", "Thi is label!!!!"));
+    }
 
 }
