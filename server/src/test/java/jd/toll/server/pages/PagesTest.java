a5cec5df9a6bf0fbea7b16c10bca7cf0f497c79c
saturn <saturn900@gmail.com>
2016-10-18 05:28:26 +0700

    ++ test for pages

---
+++ server/src/test/java/jd/toll/server/pages/PagesTest.java
@@ -0,0 +1,35 @@
+package jd.toll.server.pages;
+
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by saturn on 18.10.2016.
+ */
+public class PagesTest {
+
+    private static WicketTester tester;
+
+    @Before
+    public void init() {
+        getTester();
+    }
+
+    WicketTester getTester() {
+
+        if (tester == null) {
+            tester = new WicketTester();
+        }
+        return tester;
+    }
+
+    @Test
+    public void homePageTest() throws Exception {
+        tester.startPage(Home.class);
+        tester.assertRenderedPage(Home.class);
+    }
+
+}
