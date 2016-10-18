735c3bbff45da6b619d59c9eb4a53272de146499
saturn <saturn900@gmail.com>
2016-10-18 07:49:52 +0700

    ++ spring integration with XML

--- server/src/main/java/jd/toll/server/WicketApplication.java
+++ server/src/main/java/jd/toll/server/WicketApplication.java
@@ -4,11 +4,16 @@ import jd.toll.server.pages.Home;
 import jd.toll.server.pages.HomeExtender;
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 /**
  * Created by saturn on 17.10.2016.
  */
-public class WicketApplication extends WebApplication {
+public class WicketApplication extends WebApplication implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
+
     @Override
     public Class<? extends Page> getHomePage() {
         return Home.class;
@@ -17,5 +22,10 @@ public class WicketApplication extends WebApplication {
     @Override
     protected void init() {
         mountPage("/extender", HomeExtender.class);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
