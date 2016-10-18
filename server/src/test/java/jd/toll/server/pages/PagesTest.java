40f63c28b2fe98346e1471d3ad391ae663df29db
saturn <saturn900@gmail.com>
2016-10-18 08:08:17 +0700

    modifying test to run with Spring

--- server/src/test/java/jd/toll/server/pages/PagesTest.java
+++ server/src/test/java/jd/toll/server/pages/PagesTest.java
@@ -1,14 +1,24 @@
 package jd.toll.server.pages;
 
+import jd.toll.server.config.ServerContext;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by saturn on 18.10.2016.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ServerContext.class,
+        loader = AnnotationConfigContextLoader.class)
+@ActiveProfiles("test")
 public class PagesTest {
 
     private static WicketTester tester;
