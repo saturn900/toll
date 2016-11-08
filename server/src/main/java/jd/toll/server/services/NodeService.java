664fb893e7c82f29cc3ab1a797daa7e0e526ff64
saturn <saturn900@gmail.com>
2016-11-08 08:54:25 +0700

    concurrency: Spring task

--- server/src/main/java/jd/toll/server/services/NodeService.java
+++ server/src/main/java/jd/toll/server/services/NodeService.java
@@ -5,6 +5,7 @@ import jd.toll.server.domain.XBeeNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -129,4 +130,10 @@ public class NodeService {
 
         return result;
     }
+
+    @Scheduled(initialDelay = 3000, fixedDelay = 5000)
+    public void sendCommand() {
+        log.info("Send command !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+
 }
